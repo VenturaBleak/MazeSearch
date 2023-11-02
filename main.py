265492e0972aa8f1cfec7f0dc7f38bee7fbd813f
VenturaHaze <ijumperer@gmail.com>
2023-11-01 18:14:26 -0700

    Benni

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from maze import Maze
-from solver import BFS_Solver
+from solver import BFS_Solver, DFS_Solver
 from visualizer import Visualizer
 
 if __name__ == "__main__":
-    maze = Maze(size=20)
-    solver = BFS_Solver(maze)
-    visualizer = Visualizer(maze, solver, time_step=200)
+    # create a maze
+    maze = Maze(size=10)
+
+    # # bfs solver
+    # bfs_solver = BFS_Solver(maze)
+    # visualizer = Visualizer(maze, bfs_solver, time_step=200)
+    # visualizer.visualize_solution()
+
+    # dfs solver
+    dfs_solver = DFS_Solver(maze)
+    visualizer = Visualizer(maze, dfs_solver, time_step=200)
     visualizer.visualize_solution()
